85acc101c5312a2dcf0d282ec67f40a2538be2dd
Guilherme Monte <gmonte@inf.ufsm.br>
2022-05-30 19:34:18 -0300

    fix deploy script

--- cli.js
+++ cli.js
@@ -23,18 +23,27 @@ const { argv } = yargs.usage('Usage: $0 <command> [options]').options({
 
 const deploy = async () => {
   try {
+    const scriptParams = [
+      { param: '--alias', hasValue: true }
+    ]
+
     const cliParams = [
-      { param: '--alias', hasValue: true },
       { param: '--only', hasValue: true },
       { param: '--token', hasValue: true }
     ]
 
     if (argv.alias) {
-      await execCommand('firebase use --clear', {}, cliParams)
-      await execCommand(`firebase use ${ argv.alias }`, {}, cliParams)
+      await execCommand('firebase use --clear', {}, [
+        ...scriptParams,
+        ...cliParams
+      ])
+      await execCommand(`firebase use ${ argv.alias }`, {}, [
+        ...scriptParams,
+        ...cliParams
+      ])
     }
 
-    await execCommand('firebase deploy')
+    await execCommand('firebase deploy', {}, scriptParams)
   } catch (err) {
     console.error('Error on firebase deploy', err)
     throw (err)
