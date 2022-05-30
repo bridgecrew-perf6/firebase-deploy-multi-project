24ba248fae00287e4a59007ba651b28c7ade91b2
Guilherme Monte <gmonte@inf.ufsm.br>
2022-05-30 19:28:14 -0300

    updated parameters

--- cli.js
+++ cli.js
@@ -6,13 +6,11 @@ const yargs = require('yargs')
 const { argv } = yargs.usage('Usage: $0 <command> [options]').options({
   alias: {
     default: '',
-    demandOption: false,
     describe: 'Project alias',
     nargs: 1
   },
   only: {
     default: '',
-    demandOption: false,
     describe: 'Firebase service',
     nargs: 1
   },
